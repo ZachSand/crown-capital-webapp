d1cfd3e9e8a8f4fe8632ff73c7a9f1d7ed12d953
ZachSand <zachsand@utexas.edu>
2021-11-24 15:18:14 -0600

    Baseline of website in decent condition

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,10 @@
     // Hides all but the selected div. Allows for the feel of multiple pages in a single page without scrolling
     collapse.on('shown.bs.collapse', function() {
       currentShowingDiv = $(this);
-      $('.collapse').not($(this)).collapse('hide');
+
+      if ($(this).attr('id') !== 'navbarNavDropdown') {
+        collapse.not($(this)).collapse('hide');
+      }
 
       // Modify DOM with portfolio data, use simple boolean to make sure it only happens once
       if (!portfolioDataPopulated && portfolioData.length !== 0) {
@@ -23,7 +26,7 @@
 
     // Prevent removing a div if it is the same div already showing
     collapse.on('hide.bs.collapse', function(event) {
-      if ($(this).attr('id') === currentShowingDiv.attr('id')) {
+      if ($(this).attr('id') === currentShowingDiv.attr('id') && $(this).attr('id') !== 'navbarNavDropdown') {
         event.preventDefault();
         event.stopPropagation();
         $(this).collapse('show');
@@ -37,49 +40,69 @@
 
       for (const [portfolioPageId, sheetData] of portfolioData.entries()) {
         let portfolioPage = $('#' + portfolioPageId);
-        let sheetDataValues = sheetData.values;
-
-        // Add table header rows based on data from the first row of the sheet
-        let tableHeader = portfolioPage.find('thead');
-        let tableHeaderRow = $('<tr>').appendTo(tableHeader);
-        if (sheetDataValues.length !== 0) {
-          for (let headerCol = 0; headerCol < sheetDataValues[0].length; headerCol++) {
-            tableHeaderRow.append('<th scope=\'col\' class=\'fs-5 fw-bold\'>' + sheetData.values[0][headerCol] + '</th>');
-          }
+        if (portfolioPage && portfolioPage.length !== 0) {
+          populateTableHeaders(portfolioPage, sheetData.values);
+          populateTableBody(portfolioPage, sheetData.values);
         }
-        tableHeader.append('</tr>');
+      }
+    }
 
-        let tableBody = portfolioPage.find('tbody');
-        for (let row = 1; row < sheetDataValues.length; row++) {
-          let tableBodyRow = $('<tr>').appendTo(tableBody);
-          let sheetDataRow = sheetDataValues[row];
+    function populateTableHeaders(portfolioPage, sheetDataValues) {
+      let domTableHeaders = portfolioPage.find('thead');
+      let sheetRowWithoutHyperlink = sheetDataValues.filter(sheetRow => sheetRow.length >= 2 && !sheetRow[0].includes('HYPERLINK'));
 
-          for (let tableCol = 0; tableCol < sheetDataRow.length; tableCol++) {
-            let colData = sheetDataRow[tableCol];
-            if (colData.toString().includes('HYPERLINK')) {
-              let hyperlinkData = sheetDataRow[0].split('\"');
-              colData = '<a class=\'off-white-text fs-5\' href=\'' + hyperlinkData[1] + '\' target=\'_blank>\'>';
-              if (portfolioPageId.toLowerCase().includes('escrow')) {
-                colData += '<i class="fas fa-wallet pe-2"></i>';
+      if (domTableHeaders.length !== sheetRowWithoutHyperlink.length) {
+        return;
+      }
+
+      const tableHeaderAttributes = 'scope=\'col\' class=\'w-75\'';
+      for (let domTableHeaderIndex = 0; domTableHeaderIndex < domTableHeaders.length; domTableHeaderIndex++) {
+        let tableHeaderRow = $('<tr>').appendTo(domTableHeaders[domTableHeaderIndex]);
+        for (let sheetHeaderColumnIndex = 0; sheetHeaderColumnIndex < sheetRowWithoutHyperlink[domTableHeaderIndex].length; sheetHeaderColumnIndex++) {
+          tableHeaderRow.append('<th ' + tableHeaderAttributes + '>' + sheetRowWithoutHyperlink[domTableHeaderIndex][sheetHeaderColumnIndex] + '</th>');
+        }
+        domTableHeaders.append('</tr>');
+      }
+    }
+
+    function populateTableBody(portfolioPage, sheetDataValues) {
+      let tableBodies = portfolioPage.find('tbody');
+      let currentSheetDataRow = 1;
+      for (let tableBodyCount = 0; tableBodyCount < tableBodies.length; tableBodyCount++) {
+        let foundHeader = false;
+        while (!foundHeader && currentSheetDataRow < sheetDataValues.length) {
+          if (sheetDataValues[currentSheetDataRow][0].includes('HYPERLINK')) {
+            let tableBodyRow = $('<tr>').appendTo(tableBodies[tableBodyCount]);
+            let sheetDataRow = sheetDataValues[currentSheetDataRow];
+
+            for (let tableCol = 0; tableCol < sheetDataRow.length; tableCol++) {
+              let colData = sheetDataRow[tableCol];
+              if (colData.toString().includes('HYPERLINK')) {
+                let hyperlinkData = sheetDataRow[0].split('\"');
+                colData = '<a class=\'off-white-text\' href=\'' + hyperlinkData[1] + '\' target=\'_blank>\'>';
+                if (portfolioPage.attr('id').toLowerCase().includes('escrow')) {
+                  colData += '<i class="fas fa-wallet pe-2"></i>';
+                }
+                colData += hyperlinkData[3] + '</a>';
               }
-              colData += hyperlinkData[3] + '</a>';
+              tableBodyRow.append('<td class=\'off-white-text\'>' + colData + '</td>');
             }
-            tableBodyRow.append('<td class=\'off-white-text fs-5\'>' + colData + '</td>');
+          } else {
+            foundHeader = true;
           }
-
-          tableBody.append('</tr>');
+          currentSheetDataRow++;
         }
       }
     }
   });
 
 
-  let googleSheetsBaseURL = 'https://sheets.googleapis.com/v4/spreadsheets/';
-  let spreadsheetId = '1cszCpT5bIqWKoQ1vEAe7BkOQpxzC3n3Wx3IVc2Z-Xrg/';
-  let valuesEndpoint = 'values/';
-  let sheetRange = '!A1:B50';
-  let apiKeyParam = '?key=AIzaSyCWpzF7zF1Dgk3I9G2Gbf0lfg7zs7gA7GI';
-  let valueRenderOptionParam = '&valueRenderOption=FORMULA';
+  const googleSheetsBaseURL = 'https://sheets.googleapis.com/v4/spreadsheets/';
+  const spreadsheetId = '1cszCpT5bIqWKoQ1vEAe7BkOQpxzC3n3Wx3IVc2Z-Xrg/';
+  const valuesEndpoint = 'values/';
+  const sheetRange = '!A1:B50';
+  const apiKeyParam = '?key=AIzaSyCWpzF7zF1Dgk3I9G2Gbf0lfg7zs7gA7GI';
+  const valueRenderOptionParam = '&valueRenderOption=FORMULA';
 
   function isCrownCapitalSpreadsheet(data) {
     return data
@@ -109,4 +132,5 @@
     });
 
 
-})(jQuery);
+})
+(jQuery);
